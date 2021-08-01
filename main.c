aa1131949e6ac87b9fcceb1ad1515ad9de882339
Guilherme Augusto de Souza <oguilhermesouza@protonmail.com>
2021-08-01 18:27:27 -0300

    refactoring, trying to make more simple

--- main.c
+++ main.c
@@ -1,64 +1,42 @@
+// TODO: Make registervalue know what is the last registered value in the index
+// to avoid, wiping registered values. Maybe use a global variable to count
+// how many values was registered
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define MAX 1000
+char* valuearray[50][10];
 
-void menu(char*);
-// char* loadDatabase(char*);
+void menu();
+void registervalue();
 
 int main(int argc, char** argv) {
-	if(argc < 2) {
-		printf("%s", "Usage: ./invmanage {database.db}.\n");
-		fprintf(stderr, "The database was not especified.\n", 1);
-		return 1;
-	} else if(argc == 2) {
-		menu(argv[1]);
-	} else {
-		printf("%s","Usage: ./invmanage {database.db}.\n");
-		fprintf(stderr, "Only one argument is required.\n", 1);
-		return 1;
-	}
+	menu();
 	return 0;
 }
 
-void menu(char* database_path) {
+void menu() {
 	int option;
-	puts("------- Invetory Management -------");
-	puts("Options: 1 - Register, 2 - Remove, 3 - Logs, 4 - Exit");
-	printf("Answer: ");
+	puts("----- Inventory Management -----");
+	printf("%sAnswer: ", "1: Register\n0: Exit\n");
 	scanf("%d", &option);
-	switch(option) {
-	case 1:
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		exit(0);
-		break;
-	}
-  	// loadDatabase(database_path);
-}
-
-/*
-char* loadDatabase(char* database_path) {
-	FILE *filePointer;
-	char dataReaded[MAX];
-	filePointer = fopen(database_path, "r");
-
-	if(filePointer == NULL) {
-		puts("File failed to open.");
-	} else {
-		puts("File opened with success.");
-		while(fgets(dataReaded, MAX, filePointer) != NULL) {
-			printf("%s", dataReaded);
+	while(option != 0) {
+		switch(option) {
+		case 1:
+			registervalue();
+			break;
+		case 0:
+			exit(0);
+			break;
 		}
-		fclose(filePointer);
-		puts("Data successfully read from file");
-		puts("The file is now closed.");
+		printf("%sAnswer: ", "1: Register\n0: Exit\n");
+		scanf("%d", &option);		
 	}
-	return "None";
 }
-*/
+
+void registervalue() {
+	char* value;
+	printf("%s: ", "Value to register");
+	scanf("%s", &value);
+	valuearray[0][0] = value;
+	printf("%s\n", valuearray[0]);
+}
