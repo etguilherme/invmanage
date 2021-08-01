43134433c745ddec97dd882a951e897686308eb2
Guilherme Augusto de Souza <87988945+oguilherme-souza@users.noreply.github.com>
2021-07-31 23:18:56 -0300

    changed from json to db

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ void menu(char*);
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
-		printf("%s", "Usage: ./invmanage {database.json}.\n");
+		printf("%s", "Usage: ./invmanage {database.db}.\n");
 		fprintf(stderr, "The database was not especified.\n", 1);
 		return 1;
 	} else if(argc == 2) {
 		menu(argv[1]);
 	} else {
-		printf("%s","Usage: ./invmanage {database.json}.\n");
+		printf("%s","Usage: ./invmanage {database.db}.\n");
 		fprintf(stderr, "Only one argument is required.\n", 1);
 		return 1;
 	}
